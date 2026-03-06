4117d42472b281e7d58aac59df1f35e3d322186b
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-03-07 02:59:53 +0500

    responsive UI

--- src/ui/src/app/pages/omr/omr.ts
+++ src/ui/src/app/pages/omr/omr.ts
@@ -1,6 +1,19 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template: `<div class="card">OPENFF WORKs 😂😂</div>`
+    template: `
+        <div class="grid grid-cols-4 lg:grid-cols-12 gap-4">
+            <div class="col-span-4 lg:col-span-6 xl:col-start-2 xl:col-span-5">
+                <div class="card">
+                    open ff works
+                </div>
+            </div>
+            <div class="col-span-4 lg:col-span-6 xl:col-span-5">
+                <div class="card">
+                    open ff works
+                </div>
+            </div>
+        </div>
+    `
 })
 export class OMRComponent {}
