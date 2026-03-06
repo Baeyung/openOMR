1f0f6e7500f57d238f98f6165a77e57e6a23d104
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-03-06 21:31:38 +0500

    removing initial styling and stuff

--- src/ui/src/app/app.routes.ts
+++ src/ui/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { OMRComponent } from './components/omr/omr.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'omr',
+        component: OMRComponent
+    },
+    {
+        pathMatch: 'full',
+        redirectTo: 'omr'
+    }
+];
---
+++ src/ui/src/app/components/omr/omr.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'omr-form',
+    template: `
+        <div class="card">
+            OMR Component works!
+        </div>
+    `,
+    imports: [],
+    standalone: true
+})
+export class OMRComponent {}
